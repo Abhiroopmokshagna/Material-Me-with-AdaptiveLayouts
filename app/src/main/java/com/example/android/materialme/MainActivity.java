80cc568add98d02312692657c915d4d5229b345d
Abhiroop mokshagna <abhiroopmokshagnabheemineni@gmail.com>
2019-06-11 16:54:18 +0530

    Material Me with cardlayout,gilde,gridlayout in landscape, adaptive layouts,swipable, draggable.

--- app/src/main/java/com/example/android/materialme/MainActivity.java
+++ app/src/main/java/com/example/android/materialme/MainActivity.java
@@ -19,10 +19,14 @@ package com.example.android.materialme;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /***
  * Main Activity for the Material Me app, a mock sports news application
@@ -42,9 +46,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialize the RecyclerView.
         mRecyclerView = findViewById(R.id.recyclerView);
-
+        int gridcolcount = getResources().getInteger(R.integer.grid_column_count);
         // Set the Layout Manager.
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this,gridcolcount));
 
         // Initialize the ArrayList that will contain the data.
         mSportsData = new ArrayList<>();
@@ -55,6 +59,32 @@ public class MainActivity extends AppCompatActivity {
 
         // Get the data.
         initializeData();
+        int swipeDirs;
+        if(gridcolcount > 1){
+            swipeDirs = 0;
+        } else {
+            swipeDirs = ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;
+        }
+        ItemTouchHelper helper = new ItemTouchHelper(
+                new ItemTouchHelper.SimpleCallback(ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT |
+                        ItemTouchHelper.DOWN | ItemTouchHelper.UP,swipeDirs) {
+                    @Override
+                    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                        int from = viewHolder.getAdapterPosition();
+                        int to = target.getAdapterPosition();
+                        Collections.swap(mSportsData, from, to);
+                        mAdapter.notifyItemMoved(from, to);
+                        return true;
+                    }
+
+                    @Override
+                    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                        mSportsData.remove(viewHolder.getAdapterPosition());
+                        mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+                    }
+                }
+        );
+        helper.attachToRecyclerView(mRecyclerView);
     }
 
     /**
@@ -80,4 +110,7 @@ public class MainActivity extends AppCompatActivity {
         mAdapter.notifyDataSetChanged();
     }
 
+    public void resetSports(View view) {
+        initializeData();
+    }
 }
